3c641d4f267ce98e7b2b919d1710793f22920b02
Patrick Pajula <patrick.pajula@gmail.com>
2023-10-12 09:14:33 +0300

    First update

--- src/main/java/courses/courselist/model/CourseRepository.java
+++ src/main/java/courses/courselist/model/CourseRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
 	List<Course> findByName(String name);
+	List<Course> findByCategory(Category category);
+
 }
--- src/main/java/courses/courselist/web/CourseController.java
+++ src/main/java/courses/courselist/web/CourseController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import courses.courselist.model.Category;
 import courses.courselist.model.CategoryRepository;
 import courses.courselist.model.Course;
 import courses.courselist.model.CourseRepository;
@@ -15,42 +17,53 @@ import courses.courselist.model.CourseRepository;
 public class CourseController {
 	@Autowired
 	private CourseRepository crepository;
-	
-	@Autowired 
-	private CategoryRepository catrepository;
-	
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@RequestMapping(value = "/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/courselist")
 	public String courseList(Model model) {
 		model.addAttribute("courses", crepository.findAll());
 		return "courselist";
 	}
-	
-	@RequestMapping(value ="/programming")
-	public String programmingCourses(Model model) {
-		model.addAttribute("courses", catrepository.findAll());
-		return "programming";
-	}
-	
+
+
+
 	@RequestMapping(value = "/save")
 	public String addBook(Model model) {
 		model.addAttribute("course", new Course());
-		model.addAttribute("categorys", catrepository.findAll());
+		model.addAttribute("categorys", categoryRepository.findAll());
 		return "addcourse";
 	}
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Course course) {
 		crepository.save(course);
 		return "redirect:save";
 	}
+
+	
 	@RequestMapping(value = "/ict")
-	public String ictCourses(Model model) {
-		List<Course> ictCourses = crepository.findByName("Ict and Business");
+	public String getCoursesByCategory(Model model) {
+		Long categoryId = 1L;
+		Category ictCategory = categoryRepository.findByCategoryid(categoryId);
+		List<Course> ictCourses = crepository.findByCategory(ictCategory);
 		model.addAttribute("courses", ictCourses);
+
 		return "ict";
+	}
+	
+	@RequestMapping(value = "/programming")
+	public String programmingCourses(Model model) {
+		Long categoryId = 2L;
+		Category ictCategory = categoryRepository.findByCategoryid(categoryId);
+		List<Course> ictCourses = crepository.findByCategory(ictCategory);
+		model.addAttribute("courses", ictCourses);
+		return "programming";
 	}
 }
